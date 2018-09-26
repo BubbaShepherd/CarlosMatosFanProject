302fd4038b91cc1d4e9a387f7b001bad60eaf4fd
Hugo Alm <alhu16@nti.umea>
2018-09-26 13:40:22 +0200

    Slumpa ett tal

---
+++ spelkort/program.py
@@ -0,0 +1,9 @@
+#BitConnect
+print ("WassaWassaWassp BIT CONNEEEECT")
+print ("Välj en! **** Spader,Ruter,Hjärter eller Klöver ****")
+
+import random
+
+number = random.randint(1,4)
+
+print(number)
